3d51d257f20e04018d75598497e6c2e857bdf59c
dominhnhut01 <dominhnhut01@gmail.com>
2023-06-29 20:26:50 -0400

    add other projects

--- public/content/OtherProjects.md
+++ public/content/OtherProjects.md
@@ -1,23 +1,18 @@
-## Project 1
-Description
+## Pizza Ordering Website
+Online pizza ordering website with user authentication, CRUD functionality, real-time order updates, and responsive design
 - Tags: Category 1
 - Badges:
-  - Badge [blue]
+  - Node.js [blue]
+  - React.js [blue]
+  - SQLite [blue]
 - Buttons:
-  - Link [https://example.com]
+  - GitHub [https://github.com/dominhnhut01/online-pizze-ordering]
 
-## Project 2
-Description
+## GPS Shortest Path Application
+Interactive desktop application utilizes Dijkstra's algorithm to find the most efficient path between two vertices in a highly complex graph.
 - Tags: Category 2
 - Badges:
-  - Badge [blue]
+  - Java [blue]
+  - Java Swing GUI [blue]
 - Buttons:
-  - Link [https://example.com]
-
-## Project 3
-Description
-- Tags: Category 3
-- Badges:
-  - Badge [blue]
-- Buttons:
-  - Link [https://example.com]
+  - GitHub [https://github.com/dominhnhut01/GPS_least_cost_path]
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,17 +7,21 @@ import {
   HStack,
   Button,
   Card,
-  Badge,
   CardBody,
   Image,
   Heading,
+  SimpleGrid,
+  Badge,
+  Link,
 } from "@chakra-ui/react";
 import { Fade } from "react-reveal";
 import ProjectsArray from "./ProjectsArray";
+import OtherProjectsArray from "./OtherProjectsArray";
 
 export default function Projects({ color }) {
-    const projects = ProjectsArray();
-    
+  const projects = ProjectsArray();
+  const others = OtherProjectsArray();
+
   return (
     <>
       <Container maxW={"3xl"} id="projects">
@@ -55,10 +59,17 @@ export default function Projects({ color }) {
                       <Text py={2}>{project.description}</Text>
                       <HStack py={2}>
                         {project.buttons.map((button) => (
-                          <div key={button.text} >
-                            <Button color={`${color}.400`} onClick={() => {
-                            window.open(`${button.href}`, "_blank", "noreferrer,noopener");
-                          }}>
+                          <div key={button.text}>
+                            <Button
+                              color={`${color}.400`}
+                              onClick={() => {
+                                window.open(
+                                  `${button.href}`,
+                                  "_blank",
+                                  "noreferrer,noopener"
+                                );
+                              }}
+                            >
                               {button.text}
                             </Button>
                           </div>
@@ -74,13 +85,63 @@ export default function Projects({ color }) {
                           </Badge>
                         ))}
                       </HStack>
-                      
                     </CardBody>
                   </Stack>
                 </Card>
               </Fade>
             ))}
           </Stack>
+          <Text color={"gray.600"} fontSize={"xl"} px={4}>
+            Other Projects
+          </Text>
+          <SimpleGrid columns={[1, 2]} px={4} spacing={6}>
+            {others.map((other, idx) => (
+              <Fade bottom key={idx}>
+                <Card key={other.name}>
+                  <Stack>
+                    <CardBody align="left" h={[null, "40vh"]}>
+                      <Heading size="sm" h={"3rem"}>{other.name}</Heading>
+                      <Text
+                        fontSize="sm"
+                        py={2}
+                        h={"7rem"} // Set a fixed height for consistent text size
+                      >
+                        {other.description}
+                      </Text>
+                      <HStack spacing={2}>
+                        {other.buttons.map((button) => (
+                          <Link
+                            key={button.text}
+                            onClick={() => {
+                              window.open(
+                                `${button.href}`,
+                                "_blank",
+                                "noreferrer,noopener"
+                              );
+                            }}
+                            color={`${color}.400`}
+                          >
+                            {button.text}
+                          </Link>
+                        ))}
+                      </HStack>
+                      <HStack flexWrap="wrap" pt={4} spacing={2}>
+                        {other.badges.map((badge) => (
+                          <Badge
+                            my={2}
+                            key={badge.text}
+                            colorScheme={badge.colorScheme}
+                          >
+                            {badge.text}
+                          </Badge>
+                        ))}
+                      </HStack>
+                    </CardBody>
+                  </Stack>
+                </Card>
+              </Fade>
+            ))}
+          </SimpleGrid>
         </Stack>
       </Container>
     </>
